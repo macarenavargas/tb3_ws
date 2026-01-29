5fb07462790634b52970bc9c447bcf9fb86331ca
macarenavargas <macarena.vargashiguero@gmail.com>
2026-01-29 10:56:45 +0000

    keyboard node publisher fix

--- src/amr_teleoperation/amr_teleoperation/keyboard_node.py
+++ src/amr_teleoperation/amr_teleoperation/keyboard_node.py
@@ -3,24 +3,20 @@ from rclpy.node import Node
 from amr_msgs.msg import Key
 from sshkeyboard import listen_keyboard
 
-class MinimalPublisher(Node):
+
+class KeyboardNode(Node):
     """
     Nodo publicador simple que envía un mensaje 'Hello, World!' cada 500ms.
     """
+
     def __init__(self) -> None:
         # Inicializa la clase base Node con el nombre del nodo
         super().__init__("minimal_publisher")
-        
+
         # Configuración del Publisher
-        self._publisher = self.create_publisher(
-            msg_type=Key, 
-            topic="keyboard", 
-            qos_profile=10
-        )
-        
-       
-        listen_keyboard(on_press = self._listen_keyboard_callback,
-                        sequential=True)
+        self._publisher = self.create_publisher(msg_type=Key, topic="keyboard", qos_profile=10)
+
+        listen_keyboard(on_press=self._listen_keyboard_callback, sequential=True)
 
     def _listen_keyboard_callback(self, key) -> None:
         """
@@ -28,21 +24,23 @@ class MinimalPublisher(Node):
         """
         msg = Key()
         msg.key_input = key
-        
+
         # Publica el mensaje en el tópico
         self._publisher.publish(msg)
-        
+
         # Muestra el mensaje en la terminal del contenedor
         self.get_logger().info(f"Publishing: {msg.key_input}")
 
+
 def main(args=None):
     rclpy.init(args=args)
-    node = MinimalPublisher()
-    
+    node = KeyboardNode()
+
     rclpy.spin(node)
-    
+
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
--- src/amr_teleoperation/setup.py
+++ src/amr_teleoperation/setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'amr_teleoperation'
+package_name = "amr_teleoperation"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='turtlebot',
-    maintainer_email='turtlebot@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="turtlebot",
+    maintainer_email="turtlebot@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'teleoperation_node = amr_teleoperation.teleoperation_node:main'
+        "console_scripts": [
+            "teleoperation_node = amr_teleoperation.teleoperation_node:main",
+            "keyboard_node = amr_teleoperation.keyboard_node:main",
         ],
     },
 )
